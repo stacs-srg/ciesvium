fd409229117e1aaf6f0abc0f70c87c5a7fbfbe77
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2015-06-25 16:31:15 +0100

    Moved file and database utils from Digitising Scotland.

---
+++ src/main/java/uk/ac/standrews/cs/util/tools/AccessDatabaseExporter.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015 Digitising Scotland project:
+ * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ *
+ * This file is part of the module record_classification.
+ *
+ * record_classification is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with record_classification. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.standrews.cs.util.tools;
+
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.util.ExportUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Allows a MS Access database to be dumped to plain text files.
+ */
+public class AccessDatabaseExporter {
+
+    public static final String FIELD_SEPARATOR = ",";
+    public static final String FILE_SUFFIX = "csv";
+
+    private ExportUtil.Builder builder;
+    private File export_directory;
+
+    /**
+     * The main method.
+     *
+     * @param args the path to the database, followed by the path to the output directory.
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public static void main(final String[] args) throws IOException {
+
+        new AccessDatabaseExporter(args[0], args[1]).exportDatabase();
+    }
+
+    public AccessDatabaseExporter(String database_path, String output_directory_path) throws IOException {
+
+        Database database = DatabaseBuilder.open(new File(database_path));
+
+        builder = new ExportUtil.Builder(database).setDelimiter(FIELD_SEPARATOR).setFileNameExtension(FILE_SUFFIX).setHeader(true);
+        export_directory = Paths.get(output_directory_path).toFile();
+    }
+
+    public void exportDatabase() throws IOException {
+
+        builder.exportAll(export_directory);
+    }
+}
---
+++ src/test/java/uk/ac/standrews/cs/util/tools/AccessDatabaseExporterTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2015 Digitising Scotland project:
+ * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ *
+ * This file is part of the module record_classification.
+ *
+ * record_classification is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with record_classification. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.standrews.cs.util.tools;
+
+import com.healthmarketscience.jackcess.*;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Ignore
+public class AccessDatabaseExporterTest {
+
+    private static final String DATABASE_NAME = "testDatabase";
+    private static final String TABLE_NAME = "testTable";
+    private static final String EXPECTED_TABLE_FILE_NAME = "expected_database_contents";
+    private static final String ACCESS_SUFFIX = "mdb";
+
+    Path test_directory_path;
+    Path database_file_path;
+    Path table_file_path;
+    Path expected_table_file_path;
+
+    @Before
+    public void setUp() throws IOException, URISyntaxException {
+
+        test_directory_path = Files.createTempDirectory(null);
+        database_file_path = test_directory_path.resolve(appendSuffix(DATABASE_NAME, ACCESS_SUFFIX));
+
+        table_file_path = test_directory_path.resolve(appendSuffix(TABLE_NAME, AccessDatabaseExporter.FILE_SUFFIX));
+        expected_table_file_path = Paths.get(getClass().getResource(appendSuffix(EXPECTED_TABLE_FILE_NAME, AccessDatabaseExporter.FILE_SUFFIX)).toURI());
+    }
+
+    @Test
+    public void dumpedDatabaseFileContainsExpectedContents() throws IOException {
+
+        Database database = createDatabase();
+        createTable(database);
+        dumpTableToFile();
+
+        FileManipulation.assertThatFilesHaveSameContent(table_file_path, expected_table_file_path);
+    }
+
+    private Database createDatabase() throws IOException {
+
+        return DatabaseBuilder.create(Database.FileFormat.V2010, database_file_path.toFile());
+    }
+
+    private void createTable(Database database) throws IOException {
+
+        Table table = new TableBuilder(TABLE_NAME).addColumn(new ColumnBuilder("ID", DataType.INT)).addColumn(new ColumnBuilder("Foo", DataType.TEXT)).addColumn(new ColumnBuilder("Bar", DataType.TEXT)).toTable(database);
+
+        table.addRow(0, "foo", "bar");
+        table.addRow(1, "foo1", "bar1");
+        table.addRow(2, "foo2", "bar2");
+    }
+
+    private void dumpTableToFile() throws IOException {
+
+        AccessDatabaseExporter dumper = new AccessDatabaseExporter(database_file_path.toString(), test_directory_path.toString());
+        dumper.exportDatabase();
+    }
+
+    private String appendSuffix(String database_name, String suffix) {
+
+        return database_name + "." + suffix;
+    }
+}
