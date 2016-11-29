15b00265883e5c3088108a9af0bf2cf9db1cbc0e
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2016-11-29 22:31:12 +0000

    Fixed licences.

--- src/main/java/uk/ac/standrews/cs/util/tools/ProgressIndicator.java
+++ src/main/java/uk/ac/standrews/cs/util/tools/ProgressIndicator.java
@@ -1,20 +1,19 @@
 /*
- * Copyright 2015 Digitising Scotland project:
+ * Copyright 2016 Digitising Scotland project:
  * <http://digitisingscotland.cs.st-andrews.ac.uk/>
  *
- * This file is part of the module util.
+ * This file is part of the module ciesvium.
  *
- * util is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * ciesvium is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * util is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * ciesvium is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with util. If not, see
+ * You should have received a copy of the GNU General Public License along with ciesvium. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-
 package uk.ac.standrews.cs.util.tools;
 
 import java.util.concurrent.atomic.*;
